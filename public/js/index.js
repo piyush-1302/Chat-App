49bf6e21f47c009cf6e6cc251b79a0d294b95b0f
piyush0901 <piy0901@gmail.com>
2018-05-25 00:00:31 +0530

    6th

--- public/js/index.js
+++ public/js/index.js
@@ -19,4 +19,25 @@ socket.on('welcoming',function(message){
 
 socket.on('newMessage',function(message){
 	console.log('New Message',message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from} : ${message.text}`);
+	jQuery('#messages').append(li);
 })
+
+/*socket.emit('createMessage',{
+	from : 'PK',
+	text : "heyyy"
+},function(data){
+	console.log('Got it!',data);
+});*/
+
+jQuery('#message-form').on('submit',function(e){
+	e.preventDefault();
+
+	socket.emit('createMessage',{
+		from : 'User',
+		text : jQuery('[name=message]').val() 
+	},function(){
+
+	});
+});
--- server/server.js
+++ server/server.js
@@ -33,13 +33,14 @@ io.on('connection',(socket)=>{
 			createdAt : new Date().getTime()
 	});
 
-	socket.on('createMessage',(message)=>{
+	socket.on('createMessage',(message,callback)=>{
 		console.log('Message',message);
-		/*io.emit('newMessage',{
+		io.emit('newMessage',{
 			from : message.from,
 			text : message.text,
 			createdAt : new Date().getTime()  
-		});*/
+		});
+		callback('This is from server');
 
 		/*socket.broadcast.emit('newMessage',{
 			from : message.from,
